6ab3208ca4d30e8fd7301c288354d637c6172286
DarthCoder-afk <seanmichaelborje179@gmail.com>
2025-07-24 09:42:32 +0800

    moved social mobile icons on mobile nav menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import { X, Menu, Github, Linkedin } from "lucide-react";
+import { X, Menu, Github, Linkedin, Instagram, Facebook } from "lucide-react";
 import { useEffect, useState } from "react";
 const navItems = [
     { name: "Home", href: "#hero" },
@@ -25,30 +25,36 @@ export const NavBar = () => {
     }, []);
 
     return <nav className={cn("fixed w-full z-40 font-semibold shadow-xs bg-[#f8fafc] transition-all duration-200",
-        Scrolled ? "py-3 bg-secondary/10 backdrop-blur-lg shadow-lg" : "py-5",
+        Scrolled ? "py-3 bg-[#f8fafc] backdrop-blur-lg shadow-lg" : "py-5",
     )}>
         <div className="container mx-auto flex items-center justify-between px-4">
-            {/* <a href="#hero" className="text-xl font-bold text-primary flex items-center">
+            <a href="#hero" className="text-xl font-bold text-primary flex items-center hidden md:flex">
                 <span className="relative z-10">
-                    <span className="text-glow text-foreground">Sean</span> 
+                    <span className="text-foreground">Sean</span> 
                 </span>
-            </a> */}
+            </a>
 
             {/* Desktop View */}
             <div className="hidden md:flex mx-auto space-x-8">
                 {navItems.map((item, key) => (
-                    <a key={key} href={item.href} className="text-foreground/80 hover:text-primary transition-colors duration-200 ">
+                    <a key={key} href={item.href} className="border-b border-transparent text-foreground/80 hover:text-primary hover:border-primary hover:border-b-2 transition-colors duration-200 ">
                         {item.name}</a>
                 ))}
             </div>
 
             {/* Social Icons */}
-            <div className="flex space-x-4 ml-6">
+            <div className="flex space-x-4 ml-6 hidden md:flex">
                 <a href="https://github.com/DarthCoder-afk" target="_blank" rel="noopener noreferrer">
                 <Github className="text-foreground/70 hover:text-primary transition" size={20} />
                 </a>
                 <a href="https://www.linkedin.com/in/seanmichaelarriolaborje" target="_blank" rel="noopener noreferrer">
                 <Linkedin className="text-foreground/70 hover:text-primary transition" size={20} />
+                </a>
+                <a href="https://www.instagram.com/sean_borje/" target="_blank" rel="noopener noreferrer">
+                <Instagram className="text-foreground/70 hover:text-primary transition" size={20} />
+                </a>
+                 <a href="https://www.facebook.com/seanmichael.borje.7/" target="_blank" rel="noopener noreferrer">
+                <Facebook className="text-foreground/70 hover:text-primary transition" size={20} />
                 </a>
             </div>
 
@@ -68,6 +74,22 @@ export const NavBar = () => {
                         <a key={key} href={item.href} className="text-foreground/80 hover:text-primary transition-colores duration-200 " onClick={() => setIsMenuOpen(false)}>
                             {item.name}</a>
                     ))}
+
+                    {/* Mobile Social Icons */}
+                    <div className="flex space-x-4 pt-8">
+                        <a href="https://github.com/DarthCoder-afk" target="_blank" rel="noopener noreferrer">
+                        <Github className="text-foreground/70 hover:text-primary transition" size={24} />
+                        </a>
+                        <a href="https://www.linkedin.com/in/seanmichaelarriolaborje" target="_blank" rel="noopener noreferrer">
+                        <Linkedin className="text-foreground/70 hover:text-primary transition" size={24} />
+                        </a>
+                        <a href="https://www.instagram.com/sean_borje/" target="_blank" rel="noopener noreferrer">
+                        <Instagram className="text-foreground/70 hover:text-primary transition" size={24} />
+                        </a>
+                        <a href="https://www.facebook.com/seanmichael.borje.7/" target="_blank" rel="noopener noreferrer">
+                        <Facebook className="text-foreground/70 hover:text-primary transition" size={24} />
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
