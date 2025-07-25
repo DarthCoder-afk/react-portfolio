d00d960da58f27dbb865613e4452204288df2933
DarthCoder-afk <seanmichaelborje179@gmail.com>
2025-07-25 11:33:41 +0800

    functional email me form

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,8 +1,38 @@
-import { Mail, MapPin, Github, Linkedin, Instagram, Facebook, SendIcon } from "lucide-react"
+import { Mail, MapPin, Github, Linkedin, Instagram, Facebook, SendIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
+import emailjs from "@emailjs/browser";
+import { useRef, useState} from "react";
+import { useToast } from "@/hooks/use-toast"
 
 
 export const ContactSection = () => {
+    const {toast} = useToast();
+    const [isSubmitting, setIsSubmitting] = useState();
+    const form = useRef();
+
+    const SendEmail = (e) => {
+        e.preventDefault();
+
+        setIsSubmitting(true);
+
+        emailjs.sendForm( import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID, form.current, import.meta.env.VITE_EMAILJS_PUBLIC_KEY).then(
+            (result) => {
+                toast({
+                    title: "Message Sent Successfuly",
+                    description: "Thank you for the message. I'll get back to you as soon as possible.",                    
+                })
+                setIsSubmitting(false);
+                form.current.reset();
+            },
+            (error) => {
+                toast({
+                    title: "Message Failed to Send",
+                    description: "Please try again later.",
+                    });
+                setIsSubmitting(false);
+            }
+        )
+    }
     return <section id="contact" className="py-24 px-4 relative">
         <div className="container mx-auto max-w-5xl">
             <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">Get In Touch</h2>
@@ -52,7 +82,7 @@ export const ContactSection = () => {
                 <div className="bg-card p-8 rounded-lg shadow-xs">
                     <h3 className="text-2xl font-semibold mb-6">Send a Message</h3>
 
-                    <form action="" className="space-y-6">
+                    <form action="" ref={form} onSubmit={SendEmail} className="space-y-6">
                         <div className="flex items-center space-x-4">
                             <label htmlFor="name" className="w-1/4 text-md font-medium mb-2">Name: </label>
                             <input type="text"
@@ -65,12 +95,22 @@ export const ContactSection = () => {
 
                         <div className="flex items-center space-x-4">
                             <label htmlFor="email" className="w-1/4 text-md font-medium mb-2">Email: </label>
-                            <input type="text"
+                            <input type="email"
                             id="email"
                             name="email"
                             required
                             className="w-3/4 px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary"
                             placeholder="juandelacruz@gmail.com" />
+                        </div>
+
+                         <div className="flex items-center space-x-4">
+                            <label htmlFor="subject" className="w-1/4 text-md font-medium mb-2">Subject: </label>
+                            <input type="text"
+                            id="subject"
+                            name="subject"
+                            required
+                            className="w-3/4 px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary"
+                            placeholder="Job Offer, Project Work, Inquiry" />
                         </div>
 
                         <div className="flex items-start space-x-4">
@@ -83,8 +123,9 @@ export const ContactSection = () => {
                             placeholder="Hello, I'd like to talk about..." />
                         </div>
 
-                        <button type="submit" className={cn("cosmic-button w-full flex items-center justify-center gap-2")}>
-                            <SendIcon size={16}/> Send Message
+                        <button type="submit" disabled={isSubmitting} className={cn("cosmic-button w-full flex items-center justify-center gap-2")}>
+                           
+                            <SendIcon size={16}/>  {isSubmitting ? "Sending..." : "Send Message"}
                         </button>
                     </form>
                 </div>
