e9576e17bb50f12a822696e39709d1fcefec8f95
DarthCoder-afk <seanmichaelborje179@gmail.com>
2025-07-28 03:27:17 +0800

    fixed bg color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import { X, Menu, Github, Linkedin, Instagram, Facebook } from "lucide-react";
 export const Footer = () => {
-    return <footer className="w-full px-4 py-6 border-t border-border text-center md:text-left bg-card relative border-t border-border mt-12 pt-8">
+    return <footer className="w-full px-4 py-6 border-t border-border text-center md:text-left bg-[#f8f9fa] relative border-t border-border mt-12 pt-8">
         <div className="flex flex-col md:flex-row items-center md:justify-center px-4 space-y-4 md:space-y-0">
             <p className="text-muted-foreground text-sm">
             Copyright © {new Date().getFullYear()} | Sean Michael Borje
